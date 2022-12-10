74d3466a44bbbfc671a2501f325623f8dd4b3c71
mickeymaruf <thegoldenpopo@gmail.com>
2022-12-10 20:57:54 +0600

    fixed contact btn alignment

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -38,7 +38,7 @@ const Contact = () => {
                     <div className="mb-6">
                         <textarea name="message" id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
                     </div>
-                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 flex items-center gap-2" disabled={loading ? true : false}>{loading ? <>Loading...<Spinner /></> : 'Submit'}</button>
+                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 flex items-center gap-2 justify-center" disabled={loading ? true : false}>{loading ? <>Loading...<Spinner /></> : 'Submit'}</button>
                 </form>
             </div>
         </div>
