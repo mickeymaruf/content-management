ccd90d43d0dbb762f060234b58cc9b754f6120cd
mickeymaruf <thegoldenpopo@gmail.com>
2022-12-11 19:34:30 +0600

    a to Link converted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,22 +22,22 @@ const Navbar = () => {
                         <li><a target="_blank" href="https://www.linkedin.com/in/mickeymaruf/"><AiFillLinkedin className='h-8 w-8' /></a></li>
                         <li><a target="_blank" href="https://www.facebook.com/mickeymaruf"><AiFillFacebook className='h-8 w-8' /></a></li>
                     </ul>
-                    <div className='md:hidden' onClick={() => setToggle(!toggle)}>
+                    <div className='md:hidden cursor-pointer' onClick={() => setToggle(!toggle)}>
                         <HiBars3BottomLeft className={`h-8 w-8 ${toggle && 'opacity-0'} duration-300`} />
                     </div>
                 </div>
             </nav>
             <div className={`md:hidden backdrop-blur-lg py-10 ${toggle ? 'right-0' : '-right-20'} top-0 fixed h-screen duration-500 flex flex-col justify-center z-50 border-l border-slate-900`}>
-                <div className='px-3 absolute top-0 left-1/2 -translate-x-1/2 pt-8' onClick={() => setToggle(!toggle)}>
+                <div className='px-3 absolute top-0 left-1/2 -translate-x-1/2 pt-8 cursor-pointer' onClick={() => setToggle(!toggle)}>
                     <HiOutlineXMark className='h-8 w-8' />
                 </div>
                 <ul className='px-3 flex flex-col items-center gap-6 text-center'>
-                    <li className='w-full bg-slate-700 text-xs font-medium p-1 -rotate-45'><a href="#home">Home</a></li>
-                    <li className='w-full bg-slate-700 text-xs font-medium p-1 -rotate-45'><a href="#projects">Projects</a></li>
-                    <li className='w-full bg-slate-700 text-xs font-medium p-1 -rotate-45'><a href="#about">About</a></li>
-                    <li className='w-full bg-slate-700 text-xs font-medium p-1 -rotate-45'><a href="#skills">Skills</a></li>
-                    <li className='w-full bg-slate-700 text-xs font-medium p-1 -rotate-45'><a href="/blog">Blogs</a></li>
-                    <li className='w-full bg-slate-700 text-xs font-medium p-1 -rotate-45'><a href="#contact">Contact</a></li>
+                    <Link to="#home"><li className='w-full bg-slate-700 text-xs font-medium p-1 -rotate-45'>Home</li></Link>
+                    <Link to="#projects"><li className='w-full bg-slate-700 text-xs font-medium p-1 -rotate-45'>Projects</li></Link>
+                    <Link to="#about"><li className='w-full bg-slate-700 text-xs font-medium p-1 -rotate-45'>About</li></Link>
+                    <Link to="#skills"><li className='w-full bg-slate-700 text-xs font-medium p-1 -rotate-45'>Skills</li></Link>
+                    <Link to="/blog"><li className='w-full bg-slate-700 text-xs font-medium p-1 -rotate-45'>Blogs</li></Link>
+                    <Link to="#contact"><li className='w-full bg-slate-700 text-xs font-medium p-1 -rotate-45'>Contact</li></Link>
                     <li><a target="_blank" href="https://github.com/mickeymaruf"><AiFillGithub className='h-8 w-8' /></a></li>
                     <li><a target="_blank" href="https://www.linkedin.com/in/mickeymaruf/"><AiFillLinkedin className='h-8 w-8' /></a></li>
                     <li><a target="_blank" href="https://www.facebook.com/mickeymaruf"><AiFillFacebook className='h-8 w-8' /></a></li>
