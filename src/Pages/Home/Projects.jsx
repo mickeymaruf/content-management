614228949e2d51acb8e48fd386accc57f4b3bede
Md Maruf Hossain <mickeymaruf@gmail.com>
2023-01-05 00:00:32 +0600

    new project added (staff deck - HRM software)

--- src/Pages/Home/Projects.jsx
+++ src/Pages/Home/Projects.jsx
@@ -23,6 +23,10 @@ import pirates from '../../assets/projects/pirates.png';
 import pirates2 from '../../assets/projects/pirates2.png';
 import pirates3 from '../../assets/projects/pirates3.png';
 
+import hrms from '../../assets/projects/hrms.png';
+import hrms2 from '../../assets/projects/hrms2.png';
+import hrms3 from '../../assets/projects/hrms3.png';
+
 import "swiper/css";
 import "swiper/css/pagination";
 import { Link } from 'react-router-dom';
@@ -30,24 +34,9 @@ import { Link } from 'react-router-dom';
 const projects = [
     {
         _id: 1,
-        name: 'Pharma - Clinic management app',
-        image: [pharma, pharma2, pharma3],
-        desc: 'Designed and developed a whole doctors service platform to manage patients and visiting. Implemented appointment booking system according to the date and time slots. Implemented role- based functionality and added Firebase for authentication.',
-        features: [
-            'Designed and developed a whole doctors service platform to manage patients and visiting',
-            'Implemented appointment booking system according to the date and time slots',
-            'Restricted users from booking multiple slots at a day, but eligible to book different services',
-        ],
-        technologies: 'React(Vite), Node.js, Express.js, MongoDB, Firebase, Tailwind CSS',
-        clientCode: 'https://github.com/mickeymaruf/pharma',
-        serverCode: 'https://github.com/mickeymaruf/pharma//tree/server',
-        live: 'https://pharma-1cc98.web.app//'
-    },
-    {
-        _id: 2,
         name: 'Daily Deals e-commerce application (MERN)',
         image: [dailydeals, dailydeals2, dailydeals3],
-        desc: ' Designed and developed a fully functional buying and selling used laptops and accessories website. It is a complete e-commerce website where sellers can sell and buyers can buy their products. Dashboard features added.',
+        desc: 'Designed and developed a fully functional buying and selling used laptops and accessories website. It is a complete e-commerce website where sellers can sell and buyers can buy their products. Dashboard features added.',
         features: [
             'Designed and developed a dashboard for different users role(seller, buyer and admin)',
             'Implemented Advertisement features for sellers to advertise their products for better reach',
@@ -59,7 +48,49 @@ const projects = [
         live: 'https://dailydeals-37e5b.firebaseapp.com/'
     },
     {
+        _id: 2,
+        name: 'Staff Deck - An HR management software',
+        image: [hrms, hrms2, hrms3],
+        desc: 'An HR management software that helps companies to organize employee databases, Time & Attendance, Talent Onboarding, Managing Events, and more. The goal of Staff Deck is to make it easy for HR to streamline their processes and improve efficiency.',
+        features: [
+            'Visual representation of company\'s growth, evaluation and statistics.',
+            'Hiring and Recruiting',
+            'Department & Employee Management',
+            'Payroll System',
+            'Admin authorization',
+            'Event Management',
+            'Company\'s Home Page',
+            'Careers/Job Portal',
+        ],
+        technologies: 'React, Redux, Node.js, Express.js, MongoDB, Firebase Auth, Firebase Admin SDK, Tailwind CSS (Flowbite)',
+        clientCode: 'https://github.com/Rubayet-billah/hr-management-client',
+        serverCode: 'https://github.com/Rubayet-billah/hr-management-server',
+        live: 'https://hr-management-client-2a430.web.app/',
+        highlight: 'Group Project',
+        myContribution: [
+            'Designed and developed the front page of the dashboard with different charts that help HRs to observe the evaluation and statistics of the company.',
+            'Integrated Firebase Admin SDK for managing permissions for admins and super admins.',
+            'Implemented calendar feature for scheduling events of the company.',
+            'Designed the Careers page, Account settings, etc.',
+        ]
+    },
+    {
         _id: 3,
+        name: 'Pharma - Clinic management app',
+        image: [pharma, pharma2, pharma3],
+        desc: 'Designed and developed a whole doctors service platform to manage patients and visiting. Implemented appointment booking system according to the date and time slots. Implemented role- based functionality and added Firebase for authentication.',
+        features: [
+            'Designed and developed a whole doctors service platform to manage patients and visiting',
+            'Implemented appointment booking system according to the date and time slots',
+            'Restricted users from booking multiple slots at a day, but eligible to book different services',
+        ],
+        technologies: 'React(Vite), Node.js, Express.js, MongoDB, Firebase, Tailwind CSS',
+        clientCode: 'https://github.com/mickeymaruf/pharma',
+        serverCode: 'https://github.com/mickeymaruf/pharma//tree/server',
+        live: 'https://pharma-1cc98.web.app/'
+    },
+    {
+        _id: 4,
         name: 'Quizlet - Online Quiz (React)',
         image: [quizlet, quizlet1, quizlet2],
         desc: 'An online quiz application. You will find quizzes on different topics about Programming concepts. You can start any quiz and it will show you right/wrong based on your selection. There is a answer reveal button to reveal the correct answer.',
@@ -73,7 +104,7 @@ const projects = [
         live: 'https://quizlet1.netlify.app/'
     },
     {
-        _id: 4,
+        _id: 5,
         name: 'Browns Kitchen - Food service review app',
         image: [brownsKitchen, brownsKitchen2, brownsKitchen3],
         desc: 'A homemade food delivery service review website. Users can leave reviews on any particular dish. A new dish can be added. Users can edit/update, delete their comments and rate a dish using starts. Firebase authentication added.',
@@ -88,7 +119,7 @@ const projects = [
         live: 'https://browns-kitchen.web.app/'
     },
     {
-        _id: 5,
+        _id: 6,
         name: 'Pirates of Programming Educational Website',
         image: [pirates, pirates2, pirates3],
         desc: 'It is a educational website where a student can enroll any courses. Course materials have detail information in detail page and courses are listed unser categories. React, React-router-dom, Firebase authentication, Private route added.',
--- src/Pages/Projects/Project.jsx
+++ src/Pages/Projects/Project.jsx
@@ -5,10 +5,10 @@ import { AiOutlineArrowLeft } from 'react-icons/ai'
 
 const Project = () => {
     const location = useLocation();
-    const { name, image, desc, features, technologies, clientCode, serverCode, live } = location.state;
+    const { name, image, desc, features, technologies, clientCode, serverCode, live, highlight, myContribution } = location.state;
     return (
         <div className="bg-slate-900 text-white min-h-screen p-10 md:p-20 ">
-            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 items-stretch gap-5'>
                 <img src={image[0]} alt="" />
                 <img src={image[1]} alt="" />
                 <img src={image[2]} alt="" />
@@ -21,7 +21,7 @@ const Project = () => {
                 }
                 <a target="_blank" href={live}><button className='btn bg-blue-600 text-xs font-medium mr-4'>Live Site</button></a>
             </div>
-            <h1 className='text-4xl mt-5 mb-5'>{name}</h1>
+            <h1 className='text-4xl mt-5 mb-5'>{name} {highlight && `(${highlight})`}</h1>
             <p className='text-xl'>{desc}</p>
             <h3 className='text-2xl font-medium mt-5'>Features</h3>
             <ul className='list-disc ml-10 text-xl'>
@@ -29,6 +29,17 @@ const Project = () => {
                     features?.map((feature, idx) => <li key={`feature${idx}`}>{feature}</li>)
                 }
             </ul>
+            {
+                myContribution &&
+                <>
+                    <h3 className='text-2xl font-medium mt-5'>My Contributions</h3>
+                    <ul className='list-disc ml-10 text-xl'>
+                        {
+                            myContribution?.map((i, idx) => <li key={`feature${idx}`}>{i}</li>)
+                        }
+                    </ul>
+                </>
+            }
             <h3 className='text-2xl font-medium mt-5'>Technologies:</h3>
             <ul className='ml-10 flex flex-wrap gap-3 mt-2'>
                 {
